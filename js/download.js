cac75f0d54868dd609e6e302b16eef2b9690b17a
Ruofan Ding <rding318@gmail.com>
2014-12-02 14:14:10 +0100

    fix download feature

--- js/download.js
+++ js/download.js
@@ -2,28 +2,32 @@ angular.module('download', []).factory('downloadController', ['$q', function($q)
 
     var rootRef = new Firebase("http://cityknowledge.firebaseio.com");
 
-              /**                                                                                                                                                               
-                 * @param formId the id of the form                                                                                                                               
-                 * @return AsyncValue<formData>                                                                                                                                   
-                 */                                                                                                                                                               
-   
-    function getDataId(groupName){
-        var groupRef = rootRef.child("groups").child(groupName).child("members");
-        var deferred = $q.defer();
-        
-        groupRef.once('value', function(dataSnapshot){
-            var idArray = [];
-            dataSnapshot.forEach(function(childSnapshot){
-                idArray.push(childSnapshot.val());
-            });
-            deferred.resolve(idArray);
-        });
-        return deferred.promise;
-    }
-
+    /**
+    * @param groupName the name of group to be retrieved.
+    * @return AsyncValue<object> {groupName:" " ,data:[] }
+    *                               data is an array contains raw data in json format
+    */
     function getData(groupName){
         var dataRef = rootRef.child("data");
         var deferred = $q.defer();
+
+        /**
+        * @param groupName the name of group to be retrieved.
+        * @return AsyncValue<idArray> an array contains ids which are keys to retrieve from rootRef.child("data")
+        */                                                                                                                                                               
+        function getDataId(groupName){
+            var groupRef = rootRef.child("groups").child(groupName).child("members");
+            var deferred = $q.defer();
+            
+            groupRef.once('value', function(dataSnapshot){
+                var idArray = [];
+                dataSnapshot.forEach(function(childSnapshot){
+                    idArray.push(childSnapshot.val());
+                });
+                deferred.resolve(idArray);
+            });
+            return deferred.promise;
+        }
         
         getDataId(groupName).then(function(idArray){
             var dataArray = [];
@@ -40,24 +44,57 @@ angular.module('download', []).factory('downloadController', ['$q', function($q)
         return deferred.promise;
     }
 
-    function JSONtoCSVHeading(data){
-        var line = "";
-        for(key in data){
-            if(line != "") line += ',';
-            line += key;
-        }
-        return line;
-    }
+    
 
     function convertToCSV(groupData){
+
+        function JSONtoCSVHeading(keys){
+            var line = "";
+            for(i in keys){
+                if(line != "") line += ',';
+                line += keys[i];
+            }
+            return line;
+        }
+
+        function JSONtoCSV(json, keys, i) {
+            var line = '';
+
+            for (var i in keys) {
+                if(line != '') line += ',';
+                if (typeof json[keys[i]] === 'undefined') {
+                    //if the tuple doesn't have this property, then skip this.
+                    continue;
+                }
+                var jsonEntry = "" + json[keys[i]];
+                jsonEntry = jsonEntry.replace(/"/g, '""');
+
+                if (jsonEntry.indexOf(',') != -1) {
+                    // if the string has commas in it, surround it in quotes 
+                    line += '"' + jsonEntry + '"';
+                }else {
+                    line += jsonEntry;
+                }
+            }
+            return line;
+        }
+
         var groupName = groupData.groupName;
         var members = groupData.data;
         var csv = ""
-        headings = members[0].data;
+        var keys = []; //the column titles
 
-        csv += JSONtoCSVHeading(headings) + "\n";
         for(i in members){
-            csv += JSONtoCSV(members[i].data, headings, i) + "\n";
+            for(key in members[i].data){
+                if($.inArray(key, keys) == -1){
+                    keys.push(key);
+                }
+            }
+        }
+
+        csv += JSONtoCSVHeading(keys) + "\n";
+        for(i in members){
+            csv += JSONtoCSV(members[i].data, keys, i) + "\n";
         }
 
         //var encodedUri = 'data:attachment/csv,' + encodeURI(csv);
@@ -70,28 +107,7 @@ angular.module('download', []).factory('downloadController', ['$q', function($q)
         hiddenElement.click();
     }
 
-    function JSONtoCSV(json, keys, i) {
-        //console.log(data);
-        var line = '';
-        for (var key in keys) {
-            if(line != '') line += ',';
-            if (typeof json[key] === 'undefined') {
-                console.log(i.toString() + key);
-                line += ',';
-                continue;
-            }
-            var jsonEntry = "" + json[key];
-            jsonEntry = jsonEntry.replace(/"/g, '""');
-
-            if (jsonEntry.indexOf(',') != -1) {
-                // if the string has commas in it, surround it in quotes 
-                line += '"' + jsonEntry + '"';
-            }else {
-                line += jsonEntry;
-            }
-        }
-        return line;
-    }
+    
 
     var download = function(groupName){
         getData(groupName).then(convertToCSV);
